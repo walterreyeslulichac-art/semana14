d7a447f4b01b4d00f505fb7cc23c911e81229f87
walter reyes <walterreyeslulichac@gmail.com>
2026-07-11 23:42:58 -0500

    feat: Creacion de la clase Plan datos negocio

---
+++ src/PlanDatosAcademico.java
@@ -0,0 +1,26 @@
+public class PlanDatosAcademico extends Plan{
+    public PlanDatosAcademico() {
+        nombre = "ACADEMICO";
+    }
+    @Override
+    public void setTasa(String proveedor) {
+       switch (proveedor.toUpperCase()){
+           case "LINEARAPIDA":
+               tasa = 1.0f;
+               break;
+
+           case "CLARO":
+               tasa = 1.2f;
+               break;
+
+           case "MOVISTAR":
+               tasa = 1.25f;
+               break;
+
+           default:
+               System.out.println("Proveedor no válido.");
+               tasa = 0;
+               break;
+       }
+    }
+}
